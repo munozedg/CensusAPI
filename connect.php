54c9d8d8f01ebe4bd32f604339fed371a0409187
royhobbstn <royhobbstn@yahoo.com>
2015-02-23 10:44:03 -0700

    Initial Commit
    V 0.1 API files

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+//file with connection information
+//setup like:
+$server="54.69.15.55";
+$user="codemog";
+$password="demography";
+
+?>
---
+++ timeseriesmeta.js
@@ -0,0 +1,55 @@
+{ "data": [
+  {
+  "table": "population",
+  "description": "Total Population 1980-2010",
+  "years": [
+    {"c1980": "sf1"},
+    {"c1990": "sf1"},
+    {"c2000": "sf1"},
+    {"c2010": "data"},
+    {"acs0812": "none"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "pop1980", "cdesc": "Population 1980"},
+      {"year": "c1990", "colname": "pop1990", "cdesc": "Population 1990"},
+      {"year": "c2000", "colname": "pop2000", "cdesc": "Population 2000"},
+      {"year": "c2010", "colname": "pop2010", "cdesc": "Population 2010"}      
+    ]
+  },{
+  "table": "population2",
+  "description": "Population Time Series ACS 5Y 2008-2012 to 2009-2013",
+  "years": [
+    {"c1980": "none"},
+    {"c1990": "none"},
+    {"c2000": "none"},
+    {"c2010": "none"},    
+    {"acs0812": "data"},
+    {"acs0913": "data"}    
+    ],
+  "columnmeta": [
+      {"year": "acs0812", "colname": "popacs0812", "cdesc": "Population 2008-2012 5Y"},
+      {"year": "acs0913", "colname": "popacs0913", "cdesc": "Population 2009-2013 5Y"}     
+    ]
+  },{
+  "table": "mhi",
+  "description": "Median Household Income 1980-2010",
+  "years": [
+    {"c1980": "sf3"},
+    {"c1990": "sf3"},
+    {"c2000": "sf3"},
+    {"c2010": "none"},
+    {"acs0812": "data"},
+    {"acs0913": "none"}  
+    ],
+  "columnmeta": [
+      {"year": "c1980", "colname": "mhi1980", "cdesc": "Median Household Income 1980"},
+      {"year": "c1990", "colname": "mhi1990", "cdesc": "Median Household Income 1990"},
+      {"year": "c2000", "colname": "mhi2000", "cdesc": "Median Household Income 2000"},
+      {"year": "acs0812", "colname": "mhi0812", "cdesc": "Median Household Income 2010"}      
+    ]
+  }
+
+
+]
+}
