dff8a2b6faeada4913e39c01e65f2abc9f288e2c
Daniel Trone <royhobbstn@yahoo.com>
2015-02-26 17:28:02 +0000

    change connect.php to use correct server

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
 
-//file with connection information
-//setup like:
-$server="54.69.15.55";
+//distinguish between server in nitrous dev environment (use official AWS Database)
+//vs Server Elsewhere (use local database)
+$dev = substr($_SERVER['SERVER_NAME'],0,10);
+if($dev=='codemogapi'){$server="54.69.15.55";}else{$server=$_SERVER['SERVER_NAME'];}
 $user="codemog";
 $password="demography";
 
