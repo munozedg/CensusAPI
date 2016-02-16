d98ae79f192e79d2db3fc971bd9a56a0c347f748
Daniel Trone <royhobbstn@yahoo.com>
2016-02-16 20:52:02 +0000

    test

--- geojson.php
+++ geojson.php
@@ -8,9 +8,6 @@ header("Access-Control-Allow-Origin: *");
   //  header("Pragma: no-cache"); // HTTP 1.0
   //  header("Expires: 0"); // Proxies
 
-
-require 'connect.php';
-
 //file with connection information
 //setup like:
 //$server="server";
@@ -80,9 +77,6 @@ $moe='no';
 if($db=='acs0812' or $db=='acs0913' or $db=='acs1014'){if (isset($_GET['moe'])){$moe=make_safe($_GET['moe']);}}
 
 
-//if database is most current (now acs1014) port is 5433 to take advantage of ssd
-$port="5432";
-if($db=='acs1014'){$port="5433";}else{$port="5432";}
 
 //variables and arrays to use later
   $tablelist=[]; //array of all tables used in query
@@ -121,6 +115,10 @@ if($zoom==16){$tolerance=0.0001;}
 
 //echo $_SERVER['SERVER_ADDR'];
 // attempt a connection
+$server="104.197.26.248";
+$user="codemog";
+$password="demography";
+$port="5432";
 $dbh = pg_connect("host=".$server." port=".$port." dbname=".$db." user=".$user." password=".$password);
 
 if (!$dbh) {
